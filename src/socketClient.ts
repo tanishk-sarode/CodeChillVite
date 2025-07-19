e7d8c1a2f75c6dbf52f25d687e351bf51ac829f7
tanishksarode <tanishksarode9142@gmail.com>
2025-07-19 12:40:14 +0530

    Update socketClient to use environment variable for backend URL and enable websocket transport.

--- src/socketClient.ts
+++ src/socketClient.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('http://localhost:3001'); // Updated port
+const socket: Socket = io(import.meta.env.VITE_BACKEND_URL, { transports: ['websocket'] });
 
 export default socket; 
